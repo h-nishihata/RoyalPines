b35aedf230fa25425eeeb9297810895c7b5672b6
hitoshi nishihata <hitoshi@LAB-N728-2.local>
2016-06-05 18:30:22 +0900

    created prototype of flower field

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+
+	[SerializeField] Transform[] scenes;
+
+	// Use this for initialization
+	void Start () {
+		scenes[0].gameObject.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("1")) {
+			SwitchScene (1);
+		}else if (Input.GetKeyDown ("2")) {
+			SwitchScene (2);
+		}else if (Input.GetKeyDown ("3")) {
+			SwitchScene (3);
+		}else if (Input.GetKeyDown ("4")) {
+			SwitchScene (4);
+		}else if (Input.GetKeyDown ("5")) {
+			SwitchScene (5);
+		}
+	}
+
+	void SwitchScene(int num){
+
+		int _num = num - 1;
+
+		scenes[_num].gameObject.SetActive (true);
+
+		for (int i = 0; i < scenes.Length; i++) {
+			if (i != _num) {
+				scenes[i].gameObject.SetActive (false);
+			}
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/plainSpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class plainSpawner : MonoBehaviour {
+
+	public Transform plane;
+	private int planeScale = 3;
+
+	// Use this for initialization
+	void Start () {
+
+		for (int i = 0; i < 10; i ++) {
+			for (int j = 0; j < 10; j ++) {
+				Transform p = Instantiate (plane) as Transform;
+				p.transform.position = new Vector3 (i*planeScale*10-150, 0.5f, j*planeScale*10-150);
+				p.transform.parent = gameObject.transform;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
